f68021845290e217ae8cc299beb5d1a4c576375b
Jet Balagtas <jetbalagtas@gmail.com>
2016-04-11 09:16:32 -0400

    set rules for environment and minify

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
-import Button from './Components/Button';
+// import Button from './Components/Button';
+//
+// If we have an anchor, render the Button component on it
+if (document.querySelectorAll('a').length) {
+    require.ensure([], () => {
+        const Button = require('./Components/Button').default;
+        const button = new Button('google.com');
 
-const button = new Button('google.com'); button.render('a');
+        button.render('a');
+    }, 'button');
+}
+
+// If we have a title, render the Header component on it
+if (document.querySelectorAll('h1').length) {
+    require.ensure([], () => {
+        const Header = require('./Components/Header');
+
+        new Header().render('h1');
+    });
+}
